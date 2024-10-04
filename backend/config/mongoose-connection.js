a14a2c05465c2b0d12e020443cf8dd5876bca28d
it-Shoeb <sshoeb577@gmail.com>
2024-10-04 19:01:24 +0530

    update json

--- backend/config/mongoose-connection.js
+++ backend/config/mongoose-connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost:27017/planet")
+  .connect("mongodb+srv://shaikhshoebmy:admin@cluster0.q2u8v.mongodb.net/planet?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => {
     console.log("mongoDB connection");
   })
